320390481a29fa9f73d9c1dbaf1a5cf8c78f1744
Fasih <fasihislam1999@gmail.com>
2023-05-18 11:40:53 -0300

    updated content

--- sysmologic22/src/App.js
+++ sysmologic22/src/App.js
@@ -1,48 +1,42 @@
-import Navbar from './components/navbar/Navbar.js';
-import './App.css';
-import Herosection from './components/herosection/Herosection.js';
+import Navbar from "./components/navbar/Navbar.js";
+import "./App.css";
+import Herosection from "./components/herosection/Herosection.js";
 // import vector from './components/img/vector.svg'
-import Seconddiv from './components/seconddiv/Seconddiv.js';
-import Aboutus from './components/aboutus/Aboutus.js';
-import Services from './components/Services/Services.js';
-import Projects from './components/projects/Projects.js';
-import Pricing from './components/pricing/Pricing.js';
-import Clients from './components/clients/Clients.js';
-import Team from './components/team/Team.js';
-import Form from './components/form/Form.js';
-import Footer from './components/Footer/Footer.js';
-import Sidebar from './components/sidebar/Sidebar.js';
-import { useState } from 'react';
 
+import Aboutus from "./components/aboutus/Aboutus.js";
+import Services from "./components/Services/Services.js";
+import Projects from "./components/projects/Projects.js";
+import Pricing from "./components/pricing/Pricing.js";
+import Clients from "./components/clients/Clients.js";
 
+import Form from "./components/form/Form.js";
+import Footer from "./components/Footer/Footer.js";
+import Sidebar from "./components/sidebar/Sidebar.js";
+import { useState } from "react";
 
 function App() {
-  const [hide, setHide] = useState(false)
-  
+  const [hide, setHide] = useState(false);
+
   const onHide = () => {
-    setHide(!hide)
-  }
-  const [form, setForm] = useState("")
+    setHide(!hide);
+  };
+  const [form, setForm] = useState("");
   return (
     <>
-    
-    <Sidebar onHide={onHide} hide={hide} />
-    <div className='top' >
-      
-      <Navbar onHide={onHide}  />
-      <Herosection/>
+      <Sidebar onHide={onHide} hide={hide} />
+      <div className="top">
+        <Navbar onHide={onHide} />
+        <Herosection />
       </div>
-      
-      <Aboutus  />
-      <Services  />
-      <Projects />
-      <Pricing setForm={setForm}  />
-      <Clients/>
-      
-      <Form form={form} setForm={setForm} />
-      <Footer/>
-      
 
+      <Aboutus />
+      <Services />
+      <Projects />
+      <Pricing setForm={setForm} />
+      <Clients />
+
+      <Form form={form} setForm={setForm} />
+      <Footer />
     </>
   );
 }
