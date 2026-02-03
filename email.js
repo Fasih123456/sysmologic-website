5bb65bb32f7b797a0441a29115c65b88305ace81
Fasih <fasihislam1999@gmail.com>
2026-02-04 00:50:05 +0400

    updated form

--- email.js
+++ email.js
@@ -1,5 +1,12 @@
 function sendMail(e) {
     e.preventDefault();
+
+    const form = document.querySelector('.contact-form');
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
     const params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -9,15 +16,19 @@ function sendMail(e) {
     };
 
     console.log(params); // confirm companyUrl is present
-
+    
+    console.log("Email sent successfully");
+    
     emailjs
         .send("service_70qiruf", "template_rssdvgp", params)
         .then(() => {
-            document.getElementById("message").innerText =
-                "Your message has been sent successfully";
+            document.getElementById("alertMessage").innerText = "Your message has been sent successfully.";
+            // clear all form fields
+            form.reset();
+            
         })
         .catch(() => {
-            document.getElementById("message").innerText =
+            document.getElementById("alertMessage").innerText =
                 "Something went wrong. Please try again.";
         });
 }
